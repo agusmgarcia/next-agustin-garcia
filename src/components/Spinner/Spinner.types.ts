aff3619c232cb0d08487ebed9cedfdec2545c953
Agus Garcia <agusss.garcia19@gmail.com>
2022-07-10 02:04:35 -0300

    feat(Spinner): add component

---
+++ src/components/Spinner/Spinner.types.ts
@@ -0,0 +1,9 @@
+type SpinnerProps = {
+  className?: string;
+  color?: string;
+  height?: string | number;
+  strokeWidth?: string | number;
+  width?: string | number;
+};
+
+export default SpinnerProps;
---
+++ src/components/Spinner/index.ts
@@ -0,0 +1,4 @@
+import Spinner from "./Spinner";
+
+export type { default as SpinnerProps } from "./Spinner.types";
+export default Spinner;
