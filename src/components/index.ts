6d33d3130bca499d4e6e9c8dcfa9f5a413128c91
Agus Garcia <agusss.garcia19@gmail.com>
2022-07-10 02:06:23 -0300

    feat: export components

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as BarsIcon } from "./BarsIcon";
+export { default as Box } from "./Box";
+export { default as Button } from "./Button";
+export { default as CloseIcon } from "./CloseIcon";
+export { default as Image } from "./Image";
+export { default as Link } from "./Link";
+export { default as Spinner } from "./Spinner";
+export { default as Typography } from "./Typography";
