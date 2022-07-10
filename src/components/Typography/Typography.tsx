c184b7496e35d7a29342aedf572c28bd37dd6dab
Agus Garcia <agusss.garcia19@gmail.com>
2022-07-10 02:06:10 -0300

    feat(Typography): add component

---
+++ src/components/Typography/Typography.tsx
@@ -0,0 +1,46 @@
+import "@fontsource/roboto/500.css";
+
+import React from "react";
+
+import styles from "./Typography.module.scss";
+import TypographyProps from "./Typography.types";
+
+export default function Typography({
+  as,
+  children,
+  className,
+  variant,
+  ...props
+}: TypographyProps) {
+  return React.createElement(
+    as ?? "p",
+    {
+      ...props,
+      className: `${styles.typography} ${getVariant(variant)} ${className}`,
+    },
+    children
+  );
+}
+
+function getVariant(variant: TypographyProps["variant"]): string {
+  switch (variant) {
+    case "2.75rem":
+      return styles.t1;
+
+    case "2rem":
+      return styles.t2;
+
+    case "1.75rem":
+      return styles.t3;
+
+    case "1.25rem":
+      return styles.t4;
+
+    case "1rem":
+    default:
+      return styles.t5;
+
+    case "0.75rem":
+      return styles.t6;
+  }
+}
---
+++ src/components/Typography/Typography.types.ts
@@ -0,0 +1,18 @@
+type TypographyProps = {
+  variant?: "2.75rem" | "2rem" | "1.75rem" | "1.25rem" | "1rem" | "0.75rem";
+} & (
+  | ({
+      as: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+    } & React.HTMLAttributes<HTMLHeadingElement>)
+  | ({
+      as?: "p";
+    } & React.HTMLAttributes<HTMLParagraphElement>)
+  | ({
+      as: "span";
+    } & React.HTMLAttributes<HTMLSpanElement>)
+  | ({
+      as: "label";
+    } & React.LabelHTMLAttributes<HTMLLabelElement>)
+);
+
+export default TypographyProps;
