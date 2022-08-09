b0120b51cb3d9d614784a4c93f27c24ad551a790
Agus Garcia <agusss.garcia19@gmail.com>
2022-08-09 07:16:27 -0300

    chore: start exporting component props

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,23 @@
 export { default as BarsIcon } from "./BarsIcon";
+export type { BarsIconProps } from "./BarsIcon";
+
 export { default as Box } from "./Box";
+export type { BoxProps } from "./Box";
+
 export { default as Button } from "./Button";
+export type { ButtonProps } from "./Button";
+
 export { default as CloseIcon } from "./CloseIcon";
+export type { CloseIconProps } from "./CloseIcon";
+
 export { default as Image } from "./Image";
+export type { ImageProps } from "./Image";
+
 export { default as Link } from "./Link";
+export type { LinkProps } from "./Link";
+
 export { default as Spinner } from "./Spinner";
+export type { SpinnerProps } from "./Spinner";
+
 export { default as Typography } from "./Typography";
+export type { TypographyProps } from "./Typography";
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,5 @@
 export { default as AppPage } from "./AppPage";
+export type { AppPageProps } from "./AppPage";
+
 export { default as HomePage } from "./HomePage";
+export type { HomePageProps } from "./HomePage";
