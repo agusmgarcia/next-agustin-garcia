5651037d6cb212dc4de21d8199512f42b198bd1c
Agus Garcia <agusss.garcia19@gmail.com>
2022-10-30 14:49:29 -0300

    fix(Link): start using NextJS Link component

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,6 +1,12 @@
+import NextJSLink from "next/link";
+
 import styles from "./Link.module.scss";
 import LinkProps from "./Link.types";
 
-export default function Link(props: LinkProps) {
-  return <a {...props} className={`${styles.link} ${props.className}`} />;
+export default function Link({ href, ...props }: LinkProps) {
+  return (
+    <NextJSLink href={href ?? "#"}>
+      <a {...props} className={`${styles.link} ${props.className}`} />
+    </NextJSLink>
+  );
 }
