43b9ddb41767e4d0a0ef874b44fe8c977283eec0
Agus Garcia <agusss.garcia19@gmail.com>
2022-12-08 20:49:35 -0300

    chore(store): change variable name

--- src/store/HomeContent.ts
+++ src/store/HomeContent.ts
@@ -3,7 +3,7 @@ import { MemoryStorageSlice } from "@agusmgarcia/swr";
 import type HomeContent from "./HomeContent.types";
 
 export default class HomeContentSlice extends MemoryStorageSlice<HomeContent> {
-  constructor(_: any, fallbackData: HomeContent) {
-    super(fallbackData);
+  constructor(_: any, initialData: HomeContent) {
+    super(initialData);
   }
 }
