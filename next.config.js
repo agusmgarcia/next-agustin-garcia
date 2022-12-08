a51b3cbf315e148d67dcf94bb4ee91d24288b5d7
Agus Garcia <agusss.garcia19@gmail.com>
2022-12-08 09:41:42 -0300

    chore: bump @agusmgarcia/eslint-config to 1.0.8

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 /** @type {import('next').NextConfig} */
 module.exports = (phase) => ({
-  eslint: { dirs: ["pages", "public", "src"] },
   reactStrictMode: true,
   trailingSlash: phase !== PHASE_DEVELOPMENT_SERVER,
 });
