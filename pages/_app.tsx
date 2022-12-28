9420844d3c23f0eda0a551dcbbe8c0391052abbf
Agus Garcia <agusss.garcia19@gmail.com>
2022-12-27 23:40:53 -0300

    fix: remove useIsomorphicLayout usage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,22 +3,8 @@ import "./_app.css";
 import type { AppProps } from "next/app";
 
 import { StoreProvider } from "#src/store";
-import { useIsomorphicLayoutEffect } from "#src/utils";
 
 export default function App({ Component, pageProps }: AppProps) {
-  useIsomorphicLayoutEffect(() => {
-    const htmls = document.getElementsByTagName("html");
-    if (htmls.length !== 1)
-      throw new Error("Multiple html's elements found in the document");
-
-    const html = htmls.item(0);
-    if (html === null) throw new Error("No html element found in the document");
-
-    const lang = pageProps._app?.lang;
-    if (lang !== undefined) html.setAttribute("lang", lang);
-    else html.removeAttribute("lang");
-  }, [pageProps._app?.lang]);
-
   return (
     <StoreProvider initialData={pageProps._app?.initialData}>
       <Component {...pageProps._component} />
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,1 @@
-export { default as useIsomorphicLayoutEffect } from "./useIsomorphicLayoutEffect";
 export { default as useMediaQuery, Viewports } from "./useMediaQuery";
