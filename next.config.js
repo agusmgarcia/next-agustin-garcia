b0d4fd4b58ca650c77dd9a5759a630790269454d
Agus Garcia <agusss.garcia19@gmail.com>
2023-01-01 16:07:07 -0300

    chore: adjust trailingSlash at export phase

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+const { PHASE_EXPORT } = require("next/constants");
 
 /** @type {import('next').NextConfig} */
 module.exports = (phase) => ({
   reactStrictMode: true,
-  trailingSlash: phase !== PHASE_DEVELOPMENT_SERVER,
+  trailingSlash: phase !== PHASE_EXPORT,
 });
