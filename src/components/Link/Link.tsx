dc5ce6de32576b57b26b2f56abcb9365a5d262ef
Agus Garcia <agusss.garcia19@gmail.com>
2023-06-03 01:49:36 -0300

    chore: bump @agusmgarcia/eslint-config to 1.0.11

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -3,10 +3,12 @@ import NextJSLink from "next/link";
 import styles from "./Link.module.scss";
 import LinkProps from "./Link.types";
 
-export default function Link({ href, ...props }: LinkProps) {
+export default function Link(props: LinkProps) {
   return (
-    <NextJSLink href={href ?? "#"}>
-      <a {...props} className={`${styles.link} ${props.className}`} />
-    </NextJSLink>
+    <NextJSLink
+      {...props}
+      className={`${styles.link} ${props.className}`}
+      href={props.href ?? "#"}
+    />
   );
 }
