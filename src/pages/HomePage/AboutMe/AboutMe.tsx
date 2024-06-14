9501b2509c10190af7ecff73241805daeba90dd6
Agus Garcia <agusss.garcia19@gmail.com>
2024-06-14 14:44:15 -0300

    feat(AboutMe): migrate it to tailwindcss

--- src/pages/HomePage/AboutMe/AboutMe.tsx
+++ src/pages/HomePage/AboutMe/AboutMe.tsx
@@ -1,10 +1,10 @@
 import { useMediaQuery } from "@agusmgarcia/react-core";
+import clsx from "clsx";
 
 import { Image, Typography } from "#src/components";
 import { useHomeContent } from "#src/store";
 
 import Section from "../Section";
-import styles from "./AboutMe.module.scss";
 import AboutMeProps from "./AboutMe.types";
 
 export default function AboutMe(props: AboutMeProps) {
@@ -15,22 +15,29 @@ export default function AboutMe(props: AboutMeProps) {
 
   return (
     <Section className={props.className} id={homeContent.aboutMe.id}>
-      <div className={styles.aboutMe}>
+      <div className="flex flex-col items-center justify-center gap-12">
         <Image
           alt={homeContent.aboutMe.image.alt}
-          className={styles.image}
+          className={clsx(
+            "rounded-full border-2 border-solid border-white object-cover",
+            "h-[224px] w-[224px] md:h-[256px] md:w-[256px]",
+          )}
           height={desktop ? 256 : 224}
           loading="eager"
           src={homeContent.aboutMe.image.src}
           width={desktop ? 256 : 224}
         />
 
-        <div className={styles.content}>
+        <div className="flex flex-col items-center justify-center gap-4">
           <Typography as="h1" variant="3rem">
             {homeContent.aboutMe.name}
           </Typography>
 
-          <Typography as="p" className={styles.description} variant="1.25rem">
+          <Typography
+            as="p"
+            className="rounded-lg border-2 border-solid border-transparent bg-black bg-opacity-40 p-4 text-center"
+            variant="1.25rem"
+          >
             {homeContent.aboutMe.description}
           </Typography>
         </div>
