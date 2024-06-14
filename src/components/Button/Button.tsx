4b1a9b64403323ae698e6853979d7c4eeeacd767
Agus Garcia <agusss.garcia19@gmail.com>
2024-06-14 12:49:11 -0300

    feat(Button): migrate it to tailwindcss

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,20 @@
-import styles from "./Button.module.scss";
+import clsx from "clsx";
+
 import ButtonProps from "./Button.types";
 
 export default function Button(props: ButtonProps) {
   return (
-    <button {...props} className={`${styles.button} ${props.className}`} />
+    <button
+      {...props}
+      className={clsx(
+        props.className,
+        "rounded-2xl border-2 border-solid border-white bg-transparent px-8 py-4 text-white",
+        "transition-[transform,_background-color] will-change-[transform,_background-color]",
+        "disabled:cursor-not-allowed",
+        "enabled:focus:scale-110 enabled:focus:bg-interface",
+        "enabled:hover:scale-110 enabled:hover:bg-interface",
+        "enabled:active:scale-95",
+      )}
+    />
   );
 }
