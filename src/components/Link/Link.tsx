5716b8fe34c5e37546ff5a985b783800454520af
Agus Garcia <agusss.garcia19@gmail.com>
2024-06-14 12:02:10 -0300

    feat(Link): migrate it to tailwindcss

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,14 +1,7 @@
 import NextJSLink from "next/link";
 
-import styles from "./Link.module.scss";
 import LinkProps from "./Link.types";
 
 export default function Link(props: LinkProps) {
-  return (
-    <NextJSLink
-      {...props}
-      className={`${styles.link} ${props.className}`}
-      href={props.href ?? "#"}
-    />
-  );
+  return <NextJSLink {...props} href={props.href ?? "#"} />;
 }
