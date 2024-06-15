a240dc396689eb867034f393d229973baa3e8ca3
Agus Garcia <agusss.garcia19@gmail.com>
2024-06-15 16:06:14 -0300

    feat(Companies): migrate it to tailwindcss

--- src/pages/HomePage/Companies/Companies.tsx
+++ src/pages/HomePage/Companies/Companies.tsx
@@ -1,10 +1,10 @@
 import { useMediaQuery } from "@agusmgarcia/react-core";
+import clsx from "clsx";
 
 import { Image, Link } from "#src/components";
 import { useHomeContent } from "#src/store";
 
 import Section from "../Section";
-import styles from "./Companies.module.scss";
 import CompaniesProps from "./Companies.types";
 
 export default function Companies(props: CompaniesProps) {
@@ -19,19 +19,32 @@ export default function Companies(props: CompaniesProps) {
       id={homeContent.companies.id}
       title={homeContent.companies.subtitle}
     >
-      <div className={styles.companies}>
+      <div
+        className={clsx(
+          "grid w-full grid-cols-1 items-center gap-2",
+          "gap-4 md:grid-cols-2",
+        )}
+      >
         {homeContent.companies.links.map((c) => (
           <Link
             key={c.href}
-            className={styles.link}
+            className={clsx(
+              "rounded-2xl border-2 border-solid border-interface bg-white p-2",
+              "md:p-4",
+              "flex items-center justify-center",
+              "transition-[transform,_box-shadow] will-change-[transform,_box-shadow]",
+              "hover:scale-105 hover:shadow-md hover:shadow-interface",
+              "focus:scale-105 focus:shadow-md focus:shadow-interface",
+              "active:scale-95",
+            )}
             href={c.href}
             target={c.target}
             title={c.title}
           >
             <Image
               alt={c.image.alt}
-              className={styles.image}
-              height={desktop ? 80 : 16}
+              className="h-[64px] w-[224px] object-contain md:h-[80px] md:w-[288px]"
+              height={desktop ? 80 : 64}
               src={c.image.src}
               width={desktop ? 288 : 224}
             />
