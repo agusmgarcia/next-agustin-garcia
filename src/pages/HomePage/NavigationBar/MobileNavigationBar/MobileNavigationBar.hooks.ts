8b79efd553705f8b341d3f5282568653b425f6ca
Agus Garcia <agusss.garcia19@gmail.com>
2024-07-18 16:04:42 -0300

    chore: bump @agusmgarcia/react-core to 2.4.2

--- src/pages/HomePage/NavigationBar/MobileNavigationBar/MobileNavigationBar.hooks.ts
+++ src/pages/HomePage/NavigationBar/MobileNavigationBar/MobileNavigationBar.hooks.ts
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export default function useMobileNavigationBar() {
   const modalRef = useRef<HTMLElement>(null);
