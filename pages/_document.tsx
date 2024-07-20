bfec07a1bb7e2f50f1aac160866396e2c6b6d9ce
Agus Garcia <agusss.garcia19@gmail.com>
2024-07-20 12:42:36 -0300

    fix: adjust icon path

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,39 +1,16 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
-import packageJSON from "package.json";
 
 export default class AppDocument extends Document {
   override render() {
     return (
       <Html
         className="scroll-smooth bg-gray-800"
-        lang={this.props.__NEXT_DATA__.props.pageProps._app?.pageLanguage}
+        lang={this.props.__NEXT_DATA__.props.pageProps._app.pageLanguage}
       >
         <Head>
-          <link
-            href="/apple-touch-icon.png"
-            rel="apple-touch-icon"
-            sizes="180x180"
-          />
-          <link
-            href="/favicon-32x32.png"
-            rel="icon"
-            sizes="32x32"
-            type="image/png"
-          />
-          <link
-            href="/favicon-16x16.png"
-            rel="icon"
-            sizes="16x16"
-            type="image/png"
-          />
-          <link href="/site.webmanifest" rel="manifest" />
-          <meta content={packageJSON.description} name="description" />
-          <meta content="Agustin Garcia's profile" property="og:title" />
-          <meta content="website" property="og:type" />
-          <meta content="https://agustin-garcia.web.app/" property="og:url" />
-          <meta content="/image.png" property="og:image" />
-          <link href="/favicon.ico" rel="icon" />
+          <link href="favicon.ico" rel="icon" />
         </Head>
+
         <body>
           <Main />
           <NextScript />
